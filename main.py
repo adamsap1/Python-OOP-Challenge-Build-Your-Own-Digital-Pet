04854ab2800b1fef5f45ba5ef6215eb980e77fbf
adamsap1 <adamsapeh35@gmail.com>
2025-04-22 12:06:22 +0100

    Python project commit _ oop

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Create your pet
+my_pet = Pet("Max")
+
+# Interact with your pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.show_tricks()
+my_pet.get_status()
